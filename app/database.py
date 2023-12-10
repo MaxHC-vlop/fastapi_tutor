aefa7420cf897652833713afe77c41edaac43eeb
Vladuslav Dragushin <maxhc@inbox.ru>
2023-12-11 01:22:00 +0300

    Fix database

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from app.config import settings
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-print(settings.db_url)
+
 engie = create_async_engine(settings.db_url)
 
 async_session_maker = sessionmaker(
